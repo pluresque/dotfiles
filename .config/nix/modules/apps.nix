5d5efe6a96621f63b79fb64af1bb17ac18c310de
pluresque <daniilfedotov@protonmail.com>
2024-06-29 18:46:47 +0200

    feat: update skhd config

--- .config/nix/modules/apps.nix
+++ .config/nix/modules/apps.nix
@@ -55,38 +55,7 @@
     enable = true;
     package = pkgs.skhd;
     skhdConfig = ''
-	# Focus window
-    	ctrl + alt - h : yabai -m window --focus west
-    	ctrl + alt - j : yabai -m window --focus south
-    	ctrl + alt - k : yabai -m window --focus north
-    	ctrl + alt - l : yabai -m window --focus east
-
-    	# Fill space with window
-    	ctrl + alt - 0 : yabai -m window --grid 1:1:0:0:1:1
-
-    	# Move window
-    	ctrl + alt - e : yabai -m window --display 1; yabai -m display --focus 1
-    	ctrl + alt - d : yabai -m window --display 2; yabai -m display --focus 2
-    	ctrl + alt - f : yabai -m window --space next; yabai -m space --focus next
-    	ctrl + alt - s : yabai -m window --space prev; yabai -m space --focus prev
-
-    	# Close current window
-    	ctrl + alt - w : $(yabai -m window $(yabai -m query --windows --window | jq -re ".id") --close)
-
-    	# Rotate tree
-    	ctrl + alt - r : yabai -m space --rotate 90
-
-    	# Open application
-    	ctrl + alt - enter : alacritty
-    	ctrl + alt - e : emacs
-    	ctrl + alt - b : open -a Safari
-
-        ctrl + alt - t : yabai -m window --toggle float;\
-          yabai -m window --grid 4:4:1:1:2:2
-
-        ctrl + alt - p : yabai -m window --toggle sticky;\
-          yabai -m window --toggle topmost;\
-          yabai -m window --toggle pip
+	# Focus window  
     '';
   };
 
